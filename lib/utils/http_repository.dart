1b175b06c8ae07952c874c89d1f345bca3b6cb55
BahaaPioneer <bahaaedu.teacher@gmail.com>
2023-08-12 17:41:16 +0300

    handle http repo

---
+++ lib/utils/http_repository.dart
@@ -0,0 +1,43 @@
+import 'package:dio/dio.dart';
+
+enum RequestType { get, post, delete }
+
+class HttpRepository {
+  Future<dynamic> callRequest(
+      {required RequestType requestType,
+      required String methodName,
+      Map<String, dynamic> queryParameters = const {}}) async {
+    String baseUrl = "https://helpera.app/";
+    Response response;
+
+    Dio dioClient = Dio()
+      ..options = BaseOptions(
+          baseUrl: baseUrl,
+          followRedirects: false,
+          validateStatus: (status) {
+            return true;
+          });
+
+    switch (requestType) {
+      case RequestType.get:
+        response = await dioClient.get(methodName,
+            queryParameters: queryParameters,
+            options: Options(contentType: Headers.jsonContentType));
+        break;
+      case RequestType.post:
+        response = await dioClient.post(methodName,
+            queryParameters: queryParameters,
+            options: Options(contentType: Headers.jsonContentType),
+            data: null); //TODO handle Data
+        break;
+      case RequestType.delete:
+        response = await dioClient.delete(methodName,
+            queryParameters: queryParameters,
+            options: Options(contentType: Headers.jsonContentType),
+            data: null); //TODO handle Data
+        break;
+    }
+
+    return response.data;
+  }
+}
